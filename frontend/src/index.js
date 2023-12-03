9fcf63d40d52bfa4bd1e52f83e1ec6a7bf660cff
Adan Zaqaf <muhammadadan472@gmail.com>
2023-12-03 19:11:20 +0500

    developed login page and write logic for selecting role

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {} from "./index.css";
+import Authentication from "./Components/Authentication/Authentication";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Authentication />
   </React.StrictMode>
 );
