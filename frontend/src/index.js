c5f3907a104c4ef6dfb5fea9618c3dd79a11a153
Mobin Ahmed <mobinahmed4321@gmail.com>
2023-12-07 19:57:30 +0500

    made changes in login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {} from "./index.css";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {/* <Login /> */}
+    <React.StrictMode>
+      <SignUp />
+    </React.StrictMode>
+  </>
 );
