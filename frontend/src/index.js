f4639e0975a938485574350a38527c67db409cf7
Mobin Ahmed <mobinahmed4321@gmail.com>
2023-12-12 22:37:22 +0500

    worked on buyer dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
+import Dashboard from "../src/Components/Dashboards/Dashboard";
 // import Login from "./Components/Login/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+root.render(<Dashboard />);
