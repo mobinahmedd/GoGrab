dd4b27cdfb5cb3ff80a5cb251d23c5405215d24d
Jawad Mohsin <jawadmohsin1147@gmail.com>
2023-12-22 21:01:56 +0500

    Created Cart Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client";
 // import NewDashboard from "../src/Components/Dashboards/NewDashboard";
 import Dashboard from "../src/Components/Dashboards/Dashboard";
 // import Login from "./Components/Login/Login";
+import Cart from "../src/Components/Cart/Cart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
 // root.render(<Dashboard />);
+root.render(<Cart />);
